c5bc2addefd8aef3366e93371949c7947686844f
DeraBola <96840776+DeraBola@users.noreply.github.com>
2022-05-18 16:16:10 +0100

    commit

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-const fetchAdvice = async() = > {
-    const res = await fetch("https://api.adviceslip.com/advice")
-    const data = await res.json()
+const btn = document.querySelector('.btn');
+const id_tag = document.querySelector('.advice-id');
+const advice_tag = document.querySelector('.advice');
+console.log(btn, id_tag, advice_tag);
 
-    console.log(data)
-    document.getElementById("title").innerHTML = data.slip.id
-    document.getElementById("text").innerHTML = "data.slip.advice"
-
+function onBtnClick() {
+    console.log('click');
+    fetch('https://api.adviceslip.com/advice')
+        .then(resp => resp.json())
+        .then(jsonData => {
+            id_tag.innerText = 'advice #' + jsonData['slip']['id'];
+            advice_tag.innerText = 'advice' + jsonData['slip']['advice'];
+        });
 }
-fetchAdvice()
+btn.addEventListener('click', onBtnClick);
+onBtnClick;
